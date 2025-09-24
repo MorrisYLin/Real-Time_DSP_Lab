9eade0f44c43a415defb476b9f24ccde0428a002
Morris Lin <morrislinyuan@gmail.com>
2025-09-24 17:03:03 -0500

    Commit for Morris, will merge into main, ethan you just commit over this one

--- Src/lab.c
+++ Src/lab.c
@@ -22,16 +22,20 @@ extern float32_t fft_mag[FRAME_SIZE/8];
 
 //declare variables local to this file
 uint32_t elapsed_cycles;
+// Lab 2 - phase accumulation
 float32_t current_phase = 0;
+// Lab 2 - Difference equation / Impulse Response
 float32_t w0;
 float32_t sin_w0;
 float32_t cos_w0;
 float32_t x[3] = {1,0,0}; // x[0] is the signal x[n], x[1] is the signal x[n-1], etc
-float32_t y[3] = {0,0,0}; // Same for y^
-int16_t table[16];
-uint32_t tableIndex = 0;
-
-
+float32_t y[3] = {0,0,0}; // Same for y ^
+// Lab 2 - Lookup table: sample-by-sample
+//uint32_t table_len = 16;	// For 1kHz
+uint32_t table_len = 400;	// For 440Hz
+//int16_t table[16];	// For 1kHz
+int16_t table[400];		// For 440Hz
+uint32_t i_table = 0;
 
 /*
 This function will be called once before beginning the main program loop.
@@ -39,16 +43,62 @@ This is the best place to build a lookup table.
 */
 void lab_init(int16_t* output_buffer)
 {
-	//w0 = M_PI / 8; // for f0 = 1kHz, fs = 16kHz
-	w0 = 15 * M_PI / 8; // for f0 = 15kHz, fs = 16kHz
+//	w0 = M_PI / 8; // for f0 = 1kHz, fs = 16kHz
+//	w0 = 15 * M_PI / 8; // for f0 = 15kHz, fs = 16kHz
+	w0 = (2.0 * M_PI) * (440.0 / 16000.0); // for f0 = 440Hz, fs = 16kHz
 	float32_t amplitude;
-	for (uint32_t n = 0; n < 16; n+=1)
+	for (uint32_t n = 0; n < table_len; n+=1)
 	{
 	    amplitude = arm_sin_f32(n * w0); // sin amplitude as a float within [-1, 1]
 	    table[n] = (int16_t) (OUTPUT_SCALE_FACTOR * amplitude);
 	}
+
+	for (uint32_t i_sample = 0; i_sample < FRAME_SIZE; i_sample+=1)
+	{
+	    i_table = (i_sample/2) % table_len;
+	    output_buffer[i_sample] = table[i_table]; //left
+	    i_sample += 1;
+	    output_buffer[i_sample] = 0; //right
+	}
 	return;
 }
+// Lab 2 - Lookup table: sample-by-sample
+//void lab_init(int16_t* output_buffer)
+//{
+//	w0 = M_PI / 8; // for f0 = 1kHz, fs = 16kHz
+////w0 = 15 * M_PI / 8; // for f0 = 15kHz, fs = 16kHz
+//	float32_t amplitude;
+//	for (uint32_t n = 0; n < 16; n+=1)
+//	{
+//	    amplitude = arm_sin_f32(n * w0); // sin amplitude as a float within [-1, 1]
+//	    table[n] = (int16_t) (OUTPUT_SCALE_FACTOR * amplitude);
+//	}
+//	return;
+//}
+
+// Lab 2 - Lookup table : DMA
+//void lab_init(int16_t* output_buffer)
+//{
+////	w0 = M_PI / 8; // for f0 = 1kHz, fs = 16kHz
+////	w0 = 15 * M_PI / 8; // for f0 = 15kHz, fs = 16kHz
+//	w0 = (2.0 * M_PI) * (440.0 / 16000.0); // for f0 = 440Hz, fs = 16kHz
+//	float32_t amplitude;
+//	for (uint32_t n = 0; n < table_len; n+=1)
+//	{
+//	    amplitude = arm_sin_f32(n * w0); // sin amplitude as a float within [-1, 1]
+//	    table[n] = (int16_t) (OUTPUT_SCALE_FACTOR * amplitude);
+//	}
+//
+//	for (uint32_t i_sample = 0; i_sample < FRAME_SIZE; i_sample+=1)
+//	{
+//	    i_table = (i_sample/2) % table_len;
+//	    output_buffer[i_sample] = table[i_table]; //left
+//	    i_sample += 1;
+//	    output_buffer[i_sample] = 0; //right
+//	}
+//	return;
+//}
+
 
 /*
 This function will be called each time a complete frame of data is recorded.
@@ -86,42 +136,84 @@ int16_t process_left_sample(int16_t input_sample)
 {
 	tic();
     int16_t output_sample;
-    output_sample = table[tableIndex];
-    tableIndex += 1;
-    if (tableIndex == 16) tableIndex = 0;
+
+    output_sample = table[i_table];
+    i_table += 1;
+    if (i_table == 16)
+    {
+    	i_table = 0;
+    }
+
 	elapsed_cycles = toc();
 	printf("Elapsed Cycles: %d\n", elapsed_cycles);
 	return output_sample;
 }
 
 // Lab 1
+//int16_t process_left_sample(int16_t input_sample)
+//{
+//	tic();
+//
 //	float scaled_input = input_sample * INPUT_SCALE_FACTOR;
 //	scaled_input *= scaled_input;
 //  output_sample = (int16_t)(scaled_input * OUTPUT_SCALE_FACTOR);
 //  output_sample = input_sample;
+//
+//	elapsed_cycles = toc();
+//	printf("Elapsed Cycles: %d\n", elapsed_cycles);
+//	return output_sample;
+//}
 
 // Lab 2 - phase accumulation
+//int16_t process_left_sample(int16_t input_sample)
+//{
+//	tic();
+//
 //  output_sample = OUTPUT_SCALE_FACTOR * arm_sin_f32(current_phase);
 //	current_phase += w0;
 //	if (current_phase > 2 * M_PI) current_phase -= 2 * M_PI;
+//
+//	elapsed_cycles = toc();
+//	printf("Elapsed Cycles: %d\n", elapsed_cycles);
+//	return output_sample;
+//}
 
 // Lab 2 - Difference equation / Impulse Response
-//tic();
-//float32_t output_sample;
+//int16_t process_left_sample(int16_t input_sample)
+//{
+//	tic();
+//	float32_t output_sample;
 //
-//y[0] = 2 * cos_w0 * y[1] - y[2] + sin_w0 * x[1]; // Calculate y[n]
+//	y[0] = 2 * cos_w0 * y[1] - y[2] + sin_w0 * x[1]; // Calculate y[n]
 //
-//// Update y[n-2, n-1, n] and x[n-2. n-1, n]
-//y[2] = y[1]; y[1] = y[0];
-//x[2] = x[1]; x[1] = x[0]; x[0] = 0;
+//	// Update y[n-2, n-1, n] and x[n-2. n-1, n]
+//	y[2] = y[1]; y[1] = y[0];
+//	x[2] = x[1]; x[1] = x[0]; x[0] = 0;
 //
-//output_sample = y[0] * OUTPUT_SCALE_FACTOR;
+//	output_sample = y[0] * OUTPUT_SCALE_FACTOR;
 //
-//elapsed_cycles = toc();
-//printf("Elapsed Cycles: %d\n", elapsed_cycles);
-//return (int16_t) output_sample;
+//	elapsed_cycles = toc();
+//	printf("Elapsed Cycles: %d\n", elapsed_cycles);
+//	return (int16_t) output_sample;
+//}
 
-
+// Lab 2 - Lookup table: sample-by-sample
+//int16_t process_left_sample(int16_t input_sample)
+//{
+//	tic();
+//  int16_t output_sample;
+//
+//  output_sample = table[i_table];
+//	i_table += 1;
+//	if (i_table == 16)
+//	{
+//		i_table = 0;
+//	}
+//
+//	elapsed_cycles = toc();
+//	printf("Elapsed Cycles: %d\n", elapsed_cycles);
+//	return output_sample;
+//}
 
 /*
 This function provides access to each individual sample that is incoming on the left channel.
